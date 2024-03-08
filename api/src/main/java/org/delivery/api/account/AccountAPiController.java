701a1127ad825b0af5e004bc62271e6f6d3eb105
mostermouse <whdls7946@gmail.com>
2024-03-08 17:18:46 +0900

    delivery 2

--- api/src/main/java/org/delivery/api/account/AccountAPiController.java
+++ api/src/main/java/org/delivery/api/account/AccountAPiController.java
@@ -2,6 +2,8 @@ package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
+import org.delivery.api.common.api.Api;
+import org.delivery.api.common.error.UserErrorCode;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +20,16 @@ public class AccountAPiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public AccountMeResponse me(){
-        return AccountMeResponse.builder()
-                .name("홍길동")
-                .email("a@gmail.com")
-                .registeredAt(LocalDateTime.now())
-                .build();
+    public Api<Object> me(){
+       var response = AccountMeResponse.builder()
+               .name("홍길동")
+               .email("a@gmail.com")
+               .registeredAt(LocalDateTime.now())
+               .build();
+
+
+
+        return Api.ERROR(UserErrorCode.USER_NOT_FOUND, "홍길동 이라는 사용자 없음 ");
 
     }
 }
---
+++ api/src/main/java/org/delivery/api/filter/LoggerFilter.java
@@ -0,0 +1,55 @@
+package org.delivery.api.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Slf4j
+@Component
+public class LoggerFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        var req = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        var res = new ContentCachingResponseWrapper((HttpServletResponse) response);
+
+        chain.doFilter(req, res);
+
+        //requset 정보
+
+        var hearderNames = req.getHeaderNames();
+        var heardValues = new StringBuilder();
+
+        hearderNames.asIterator().forEachRemaining(headKey -> {
+            var headerValue = req.getHeader(headKey);
+            //authorization-token : ??? , user-agent : ???
+            heardValues.append(headKey).append(" : ").append(headerValue).append(" , ");
+
+        });
+        var requstBody = new String(req.getContentAsByteArray());
+        var uri = req.getRequestURI();
+        var method = req.getMethod();
+
+
+        log.info(">>>>> uri : {}, method : {}, header : {} , body : {}", uri, method, heardValues, requstBody);
+
+
+        //response 정보
+        var responseHeaderValues = new StringBuilder();
+        res.getHeaderNames().forEach(headerKey -> {
+            var headerValue = res.getHeader(headerKey);
+            responseHeaderValues.append(headerKey).append(" : ").append(headerValue).append(" , ");
+        });
+
+        var responseBody = new String(res.getContentAsByteArray());
+        log.info("<<<<< uri : {}, method : {}, header : {} , body : {}", uri, method, responseHeaderValues, responseBody);
+
+        res.copyBodyToResponse();
+    }
+}
